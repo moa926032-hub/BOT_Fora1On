d45a11aeaebc62b15110120eea759d5cab7b1f00
𝑴𝑶 𝑭𝑶𝑹𝑨1𝑶𝑵 <moa926032@gmail.com>
2026-09-13 18:50:55 +0300

    Update newsletter details in pro.js

--- plugins/tools/pro.js
+++ plugins/tools/pro.js
@@ -1,5 +1,5 @@
 // ─── plugins/profile.js ───
-// 𝑳𝑰𝑮𝑯𝑻 ┇🩸┇ 𝑩𝑶𝑻 - Fetch Profile Picture
+// 𝑱.𝑨.𝑵 𝑩𝑶𝑻 - Fetch Profile Picture
 
 const HEADER = "❐═━━━═╊⊰🩸⊱╉═━━━═❐";
 
@@ -8,8 +8,8 @@ const context = (jid) => ({
     isForwarded: true,
     forwardingScore: 1,
     forwardedNewsletterMessageInfo: {
-        newsletterJid: '120363405545946827@newsletter',
-        newsletterName: '𝑳𝑰𝑮𝑯𝑻 ┇🩸┇ 𝑪𝑯𝑨𝑵𝑵𝑬𝑳',
+        newsletterJid:'120363401670228863@newsletter',
+        newsletterName: '𝑭𝒍𝒂𝒔𝒉𝒃𝒂𝒄𝒌',
         serverMessageId: 0
     }
 });
