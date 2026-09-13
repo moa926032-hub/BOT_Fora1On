a94b5cb14ef15819dbd3890980c9c9cae329fbff
𝑴𝑶 𝑭𝑶𝑹𝑨1𝑶𝑵 <moa926032@gmail.com>
2026-09-13 17:59:56 +0300

    Update video URL in bot.js

--- plugins/group/bot.js
+++ plugins/group/bot.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn }) => {
-    await conn.circular(m.chat, { vid: "https://files.catbox.moe/bqxzcl.mp4", sec: 200 }, m);
+    await conn.circular(m.chat, { vid: "https://files.catbox.moe/tdvci0.mp4", sec: 200 }, m);
 }
 
 handler.command = ["بوم"];
