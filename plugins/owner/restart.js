64ce90865c65104708ca00778c78c13de7412497
𝑴𝑶 𝑭𝑶𝑹𝑨1𝑶𝑵 <moa926032@gmail.com>
2026-09-13 18:20:36 +0300

    Update restart message for bot name change

--- plugins/owner/restart.js
+++ plugins/owner/restart.js
@@ -1,8 +1,8 @@
 const test = async (m, { conn, bot }) => {
   m.react("🟢")
   
-  conn.msgUrl(m.chat, "𝑳𝑰𝑮𝑯𝑻𝑩𝑶𝑻 𝑰𝑺 𝑹𝑬𝑺𝑻𝑨𝑹𝑻𝑰𝑵𝑮", { 
-    title: "𝑳𝑰𝑮𝑯𝑻 𝑩𝑶𝑻",
+  conn.msgUrl(m.chat, "𝑱.𝑨.𝑵 𝑩𝑶𝑻 𝑰𝑺 𝑹𝑬𝑺𝑻𝑨𝑹𝑻𝑰𝑵𝑮", { 
+    title: "𝑱.𝑨.𝑵 𝑩𝑶𝑻",
     body: "𝑻𝑯𝑬 𝑩𝑶𝑻 𝑰𝑺 𝑹𝑬𝑺𝑻𝑨𝑹𝑻𝑰𝑵𝑮 🔁",
     img: "https://g.top4top.io/p_3700yob0b1.jpg",
     big: false 
