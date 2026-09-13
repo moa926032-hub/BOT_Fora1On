5df8aee5b78e1aebb683d09be2e9b12b84cb04ee
𝑴𝑶 𝑭𝑶𝑹𝑨1𝑶𝑵 <moa926032@gmail.com>
2026-09-13 17:38:53 +0300

    Update newsletter information in yt.js

--- plugins/download/yt.js
+++ plugins/download/yt.js
@@ -35,8 +35,8 @@ const handler = async (m, { conn, command, text }) => {
         isForwarded: true,
         forwardingScore: 1,
         forwardedNewsletterMessageInfo: {
-          newsletterJid: '120363405545946827@newsletter',
-          newsletterName: '𝑳𝑰𝑮𝑯𝑻 ┇🩸┇ 𝑪𝑯𝑨𝑵𝑵𝑬𝑳',
+          newsletterJid: '120363401670228863@newsletter',
+          newsletterName: '𝑭𝒍𝒂𝒔𝒉𝒃𝒂𝒄𝒌',
           serverMessageId: 0
         }
       }
